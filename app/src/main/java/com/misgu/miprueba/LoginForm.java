23ab65f9123ab16d4f5872c9d11e63805d02ab76
jotape118 <juanpablocardonacorrea8@gmail.com>
2024-08-20 16:06:47 -0500

    V1.2 Se le agrego una segunda pantalla al momento de realizar el registro, en donde se debera de colocar el usuario y contraseña creado anteriormente.
    Tambien se agregaron contadores para cada ciclo de vida de la app.

---
+++ app/src/main/java/com/misgu/miprueba/LoginForm.java
@@ -0,0 +1,95 @@
+package com.misgu.miprueba;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Objects;
+
+public class LoginForm extends AppCompatActivity {
+    Button buttonReturn,buttonConfirm;
+    EditText editTextUser,editTextPass;
+    String strUser,strPass;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login_form);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        buttonReturn  = (Button) findViewById(R.id.buttonReturn);
+        buttonConfirm = (Button) findViewById(R.id.buttonConfirm);
+        editTextUser = (EditText) findViewById(R.id.editTextUser);
+        editTextPass = (EditText) findViewById(R.id.editTextPass);
+        Bundle in_DB =getIntent().getExtras();
+        String[] DB = in_DB.getStringArray("Key");
+
+        buttonReturn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Atras();
+            }
+        });
+        buttonConfirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                strUser= editTextUser.getText().toString();
+                strPass= editTextPass.getText().toString();
+                if (Objects.equals(strUser, DB[0])&& Objects.equals(strPass, DB[1])){
+                    AlertaLogin(DB);
+                }else {
+                    AlertaErrorLogin();
+                }
+            }
+        });
+
+    }
+
+    public void AlertaErrorLogin() {
+        AlertDialog.Builder builder=new AlertDialog.Builder(LoginForm.this);
+        builder.setTitle("Credenciales incorrectos");
+        builder.setMessage("Credenciales no validos, escriba los correctos");
+        builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(LoginForm.this, "Vuelva a intentarlo",Toast.LENGTH_SHORT).show();
+            }
+        });
+        AlertDialog alertDialog =builder.create();
+        alertDialog.show();
+    }
+
+    public void AlertaLogin(String[] DB) {
+        AlertDialog.Builder builder=new AlertDialog.Builder(LoginForm.this);
+        builder.setTitle("Login exitoso");
+        builder.setMessage("Nombre: "+DB[2]+"\nApellido: "+DB[3]+"\nCorreo: "+DB[4]);
+        builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(LoginForm.this, "So es todo",Toast.LENGTH_SHORT).show();
+            }
+        });
+        AlertDialog alertDialog =builder.create();
+        alertDialog.show();
+    }
+
+    private void Atras() {
+        Intent i= new Intent(this, MainActivity.class);
+        startActivity(i);
+    }
+}
--- app/src/main/java/com/misgu/miprueba/MainActivity.java
+++ app/src/main/java/com/misgu/miprueba/MainActivity.java
@@ -1,32 +1,45 @@
 package com.misgu.miprueba;
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.util.Log;
 import android.widget.Button;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
 import com.misgu.miprueba.databinding.ActivityMainBinding;
 
-import java.util.ArrayList;
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
     EditText editTextNombre,editTextApellido,editTextMail,editTextUser,editTextPass;
     Button buttonDell,buttonConfirm;
-    int intCount;
-    String strNombre,strApellido,strMail,strUser,strPass;
-    private ActivityMainBinding binding;
+    String strNombre,strApellido,strMail,strUser,strPass, TAG="Test";
+    Boolean boolCamposVacios;
+    Bundle enviardatos=new Bundle();
+    String[] DB= new String[5];
 
+    TextView textViewonCreate,textViewonStart,textViewonResume,textViewonPause,textViewonStop,textViewonRestart,textViewonDestroy;
+    int intonCreate=0,intonStart=0,intonResume=0,intonPause=0,intonStop=0,intonRestart=0,intonDestroy=0;
+    private ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG,"Estoy en onCreate");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textViewonCreate= (TextView) findViewById(R.id.textViewonCreate);
+        intonCreate+=1;
+
+        textViewonCreate.setText("onCreate #"+intonCreate);
 
         editTextNombre= (EditText) findViewById(R.id.editTextNombre);
         editTextApellido= (EditText) findViewById(R.id.editTextApellido);
@@ -36,40 +49,199 @@ public class MainActivity extends AppCompatActivity {
         buttonDell= (Button) findViewById(R.id.buttonDell);
         buttonConfirm= (Button) findViewById(R.id.buttonConfirm);
 
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        textViewonStart= (TextView) findViewById(R.id.textViewonStart);
+        intonStart+=1;
+
+        textViewonStart.setText("onStart #"+intonStart);
+        Log.d(TAG,"Estoy en onStart");
         //Funcion de borrado de datos de click
         buttonDell.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
-                editTextNombre.setText("");
-                editTextApellido.setText("");
-                editTextMail.setText("");
-                editTextUser.setText("");
-                editTextPass.setText("");
-
+                BorrarDatos();
             }});
         buttonConfirm.setOnClickListener(new View.OnClickListener(){
             @Override
-            public void onClick(View view){
-                strNombre=editTextNombre.getText().toString();
-                strApellido=editTextApellido.getText().toString();
-                strMail=editTextMail.getText().toString();
-                strUser=editTextUser.getText().toString();
-                strPass=editTextPass.getText().toString();
-                ShowAlertdialogue();
+            public void onClick(View view) {
+                strNombre = editTextNombre.getText().toString();
+                strApellido = editTextApellido.getText().toString();
+                strMail = editTextMail.getText().toString();
+                strUser = editTextUser.getText().toString();
+                strPass = editTextPass.getText().toString();
+                Emptyitems(boolCamposVacios);
+                if (boolCamposVacios==Boolean.FALSE){
+                    DB[0]=strUser;
+                    DB[1]=strPass;
+                    DB[2]=strNombre;
+                    DB[3]=strApellido;
+                    DB[4]=strMail;
+                    //ShowAlertDatos();
+                    login();
+                }else {
+                    ShowAlertErrorDatos();
                 }
-            });
-        // Passing each menu ID as a set of Ids because each
+            }
+        });
     }
 
-    private void ShowAlertdialogue() {
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        textViewonResume= (TextView) findViewById(R.id.textViewonResume);
+        intonResume+=1;
+
+        textViewonResume.setText("onResume #"+intonResume);
+        Log.d(TAG,"Estoy en onResume");
+        Toast.makeText(MainActivity.this, "Estoy en onResume",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        textViewonPause= (TextView) findViewById(R.id.textViewonPause);
+        intonPause+=1;
+
+        textViewonPause.setText("onPause #"+intonPause);
+        Log.d(TAG,"Estoy en onPause");
+        Toast.makeText(MainActivity.this, "Estoy en onPause",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        textViewonStop= (TextView) findViewById(R.id.textViewonStop);
+        intonStop+=1;
+
+        textViewonStop.setText("onStop #"+intonStop);
+        Log.d(TAG,"Estoy en onStop");
+        Toast.makeText(MainActivity.this, "Estoy en onStop",Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+
+        textViewonRestart= (TextView) findViewById(R.id.textViewonRestart);
+        intonRestart+=1;
+
+        textViewonRestart.setText("onRestart #"+intonRestart);
+        Log.d(TAG,"Estoy en onRestart");
+        Toast.makeText(MainActivity.this, "Estoy en onRestart",Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        textViewonCreate= (TextView) findViewById(R.id.textViewonDestroy);
+        intonDestroy+=1;
+
+        textViewonDestroy.setText("onDestroy #"+intonDestroy);
+        Log.d(TAG,"Estoy en onDestroy");
+        Toast.makeText(MainActivity.this, "Estoy en onDestroy",Toast.LENGTH_SHORT).show();
+    }
+
+
+    public void login() {
+        Intent i =new Intent(MainActivity.this, LoginForm.class);
+        enviardatos.putStringArray("Key",DB);
+        i.putExtras(enviardatos);
+        startActivity(i);
+    }
+
+    private void BorrarDatos() {
+        editTextNombre.setText("");
+        editTextApellido.setText("");
+        editTextMail.setText("");
+        editTextUser.setText("");
+        editTextPass.setText("");
+    }
+
+
+    private boolean Emptyitems(Boolean boolCamposVacios) {
+        this.boolCamposVacios=Boolean.FALSE;
+        if (Objects.equals(strNombre, "")){
+            this.boolCamposVacios =Boolean.TRUE;
+        }
+        if (Objects.equals(strApellido, "")) {
+            this.boolCamposVacios =Boolean.TRUE;
+        }
+        if (Objects.equals(strMail, "")) {
+            this.boolCamposVacios =Boolean.TRUE;
+        }
+        if (Objects.equals(strUser, "")) {
+            this.boolCamposVacios =Boolean.TRUE;
+        }
+        if (Objects.equals(strPass, "")) {
+            this.boolCamposVacios =Boolean.TRUE;
+        }
+        return this.boolCamposVacios;
+    }
+
+
+    private void ShowAlertErrorDatos() {
+        if (Objects.equals(strNombre, "")){
+            strNombre="Nombre ";
+        }else {
+            strNombre = "";
+        }
+        if (Objects.equals(strApellido, "")){
+            strApellido="Apellido ";
+        }else {
+            strApellido = "";
+        }
+        if (Objects.equals(strMail, "")) {
+            strMail="Correo ";
+        }else{
+            strMail = "";
+        }
+        if (Objects.equals(strUser, "")){
+            strUser ="Usuario ";
+        }else {
+            strUser ="";
+        }
+        if (Objects.equals(strPass, "")){
+            strPass = "Contraseña";
+        }else {
+            strPass="";
+        }
         AlertDialog.Builder builder=new AlertDialog.Builder(this);
         builder.setTitle("Alerta");
-        builder.setMessage("Datos de usuario:\n"+strNombre+"\n"+strApellido+"\n"+strMail+"\n"+strUser+"\n"+strPass);
+        builder.setMessage("Han faltado los siguientes datos:\n"+strNombre+strApellido+strMail+strUser+strPass);
 
         builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
-                Toast.makeText(MainActivity.this, "Texto de ejemplo",Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "Por favor ingresar los datos faltantes",Toast.LENGTH_SHORT).show();
+            }
+
+        });
+
+        AlertDialog alertDialog =builder.create();
+        alertDialog.show();
+    }
+
+    private void ShowAlertDatos() {
+        AlertDialog.Builder builder=new AlertDialog.Builder(this);
+        builder.setTitle("Datos de usuario");
+        builder.setMessage(DB[0]+"\n"+DB[1]+"\n"+DB[2]+"\n"+DB[3]+"\n"+DB[4]);
+
+        builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(MainActivity.this, "Registro Completado",Toast.LENGTH_SHORT).show();
             }
 
         });
